fdcec9a024b311e2acb68b647ae2311ec5b8dff3
Alex Lorenz <alex.lorenz@cwi.com.br>
2020-11-20 21:52:12 -0300

    Revert "Update VerificarEntradaService.java"
    This reverts commit 64d5a2dc5ae8ae99823209eb490da4f160559268.

--- Trabalho GA/src/VerificarEntradaService.java
+++ Trabalho GA/src/VerificarEntradaService.java
@@ -12,24 +12,28 @@ class VerificarEntradaService {
 
     String verificar(String entrada) {
         switch (entrada) {
-            case "1" -> {
+            case "1":
                 Teclado.mostraMensagem("Opção de busca por CPF selecionada!");
                 arvoreService.preBuscarCPF(Teclado.leString("Digite o CPF que deseja buscar: "));
-            }
-            case "2" -> {
+                break;
+            case "2":
                 Teclado.mostraMensagem("Opção de busca por intervalo de data de nascimento selecionada!");
                 String id = Teclado.leString("Data inicial do intervalo que deseja buscar: ");
                 Date initialDate = arvoreService.transformToDate(id);
                 String fd = Teclado.leString("Data final do intervalo que deseja buscar: ");
                 Date finalDate = arvoreService.transformToDate(fd);
                 arvoreService.preBuscaDataNascimento(initialDate, finalDate);
-            }
-            case "3" -> {
+                break;
+            case "3":
                 Teclado.mostraMensagem("Opção de busca por nome selecionada!");
                 arvoreService.preBuscaNome(Teclado.leString("Digite o nome que deseja buscar: "));
-            }
-            case "s" -> Teclado.mostraMensagem("Encerrando programa!");
-            default -> Teclado.mostraMensagem("Opção inválida!");
+                break;
+            case "s":
+                Teclado.mostraMensagem("Encerrando programa!");
+                break;
+            default:
+                Teclado.mostraMensagem("Opção inválida!");
+                break;
         }
         return entrada;
     }
